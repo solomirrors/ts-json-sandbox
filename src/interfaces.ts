39a2b8bee83a6d70e43ee37f72287eeae90e0a1d
solomirrors <solomirrors@icloud.com>
2021-12-12 22:01:22 +0100

    Interfaces TypeScript

---
+++ src/interfaces.ts
@@ -0,0 +1,33 @@
+interface Reportable {
+    summary(): string
+}
+
+const oldCivic = {
+    name: 'civic',
+    year: 2000,
+    broken: true,
+    summary(): string {
+        return `Name: ${this.name}`
+    }
+}
+
+const cola = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40,
+    summary(): string {
+        return `Cola has ${this.sugar} grams of sugar`
+    }
+}
+
+const printSummary =  (item: Reportable) : void => {
+    console.log(item.summary())
+}
+
+printSummary(oldCivic);
+printSummary(cola);
+
+
+
+
+export {oldCivic}
